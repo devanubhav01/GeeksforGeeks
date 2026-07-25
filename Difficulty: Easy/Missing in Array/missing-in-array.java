3e1f3a579325b7178fdf6c83708cff67c30e6a3d
ANUBHAV SINGH <anubhavsingh18599@gmail.com>
2026-07-25 16:35:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,15 +1,11 @@
 class Solution {
-    int missingNum(int arr[]) {
-     long m=arr.length+1;
-        
-        
-        long expectedSum=m*(m+1)/2;
-        long sum=0;
-        for(int i=0;i<arr.length;i++){
-            sum+=arr[i];
-        }
-       return (int)(expectedSum-sum); 
-        
-    }
-    
+	int missingNum(int arr[]) {
+		long n = arr.length+1;
+		long expectedSum = n*(n+1)/2;
+		long sum = 0;
+		for (int i = 0; i<arr.length; i++) {
+			sum += arr[i];
+		}
+		return (int)(expectedSum-sum);
+	}
 }
