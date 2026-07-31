539041d03096cf9c489bbfc1ca210b50e28c3a72
ANUBHAV SINGH <anubhavsingh18599@gmail.com>
2026-07-31 19:04:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.java
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -1,28 +1,16 @@
 class Solution {
     int majorityElement(int arr[]) {
-        int count=0;
-        int candidate=0;
-        for(int i=0;i<arr.length;i++){
-            if(count==0){
-                candidate=arr[i];
-            }
-           if(arr[i]==candidate){
-                count++;
-            }else{
-                count--;
+         HashMap<Integer, Integer> freq = new HashMap<>();
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            freq.put(arr[i], freq.getOrDefault(arr[i], 0) + 1);
+
+            if (freq.get(arr[i]) > n / 2) {
+                return arr[i];
             }
         }
-        int actualCount = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == candidate) {
-                actualCount++;
-            }
-        }
-        
-        if (actualCount > arr.length / 2) {  
-            return candidate;
-        }
-        
+
         return -1;
         
     }
