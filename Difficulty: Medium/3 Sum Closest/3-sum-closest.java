63166eb74549034394cd21270b4d3cc543b0964a
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-03 16:32:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/3 Sum Closest/3-sum-closest.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int closest3Sum(int[] arr, int target) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int small = Integer.MAX_VALUE;
+        int resultValue = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+                int sub = Math.abs(target - sum);
+
+                if (sub < small || (sub == small && sum > resultValue)) {
+                    small = sub;
+                    resultValue = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return resultValue;
+    }
+}
