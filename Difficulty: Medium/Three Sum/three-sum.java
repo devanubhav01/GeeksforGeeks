a43e9820e2d9c82857a70eab525a267f38ac0f80
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-03 14:34:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Three Sum/three-sum.java
+++ Difficulty: Medium/Three Sum/three-sum.java
@@ -1,43 +1,40 @@
 class Solution {
     public static ArrayList<ArrayList<Integer>> triplets(int[] arr) {
         Arrays.sort(arr);
-        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
-        int n = arr.length;
-
-        for (int i = 0; i < n - 2; i++) {
-            // Skip duplicate first elements
-            if (i > 0 && arr[i] == arr[i - 1]) {
-                continue;
-            }
-            int left = i + 1;
-            int right = n - 1;
-            while (left < right) {
-                int sum = arr[i] + arr[left] + arr[right];
-                if (sum == 0) {
-                    ArrayList<Integer> triplet = new ArrayList<>();
-                    triplet.add(arr[i]);
-                    triplet.add(arr[left]);
-                    triplet.add(arr[right]);
-                    ans.add(triplet);
-
-                    left++;
-                    right--;
-
-                    // Skip duplicate left values
-                    while (left < right && arr[left] == arr[left - 1]) {
-                        left++;
-                    }
-                    // Skip duplicate right values
-                    while (left < right && arr[right] == arr[right + 1]) {
-                        right--;
-                    }
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
-                }
-            }
-        }
-        return ans;
+        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+        int n= arr.length;
+        for(int i=0;i<arr.length;i++){
+         if(i>0&&arr[i]==arr[i-1]){
+             continue;
+         }
+         int left=i+1;
+         int right=n-1;
+         while(left<right){
+             int sum=arr[i]+arr[left]+arr[right];
+             if(sum==0){
+                 ArrayList<Integer> triplet=new ArrayList<>();
+                 triplet.add(arr[i]);
+                 triplet.add(arr[left]);
+                 triplet.add(arr[right]);
+                 ans.add(triplet);
+                 left++;
+                 right--;
+                 while(left<right &&arr[left]==arr[left-1]){
+                     left++;
+                 }
+                 while(left<right &&arr[right]==arr[right+1]){
+                     right--;
+                 }
+                 }else if(sum<0){
+                     left++;
+                 
+                 }else{
+                     right--;
+                 }
+             }
+         }
+         return ans;
+     }
+        
     }
-}
+
