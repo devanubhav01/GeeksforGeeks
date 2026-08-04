16a173a6729f45b6ad3253042de920136a6eef64
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-04 12:15:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Three Sum/three-sum.java
+++ Difficulty: Medium/Three Sum/three-sum.java
@@ -1,8 +1,8 @@
 class Solution {
     public static ArrayList<ArrayList<Integer>> triplets(int[] arr) {
+        ArrayList<ArrayList<Integer>> nums=new ArrayList<>();
         Arrays.sort(arr);
-        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
-        int n= arr.length;
+        int n=arr.length;
         for(int i=0;i<arr.length;i++){
          if(i>0&&arr[i]==arr[i-1]){
              continue;
@@ -11,30 +11,29 @@ class Solution {
          int right=n-1;
          while(left<right){
              int sum=arr[i]+arr[left]+arr[right];
+             
+             ArrayList<Integer> z=new ArrayList<>();
+             
              if(sum==0){
-                 ArrayList<Integer> triplet=new ArrayList<>();
-                 triplet.add(arr[i]);
-                 triplet.add(arr[left]);
-                 triplet.add(arr[right]);
-                 ans.add(triplet);
+                 z.add(arr[i]);
+                 z.add(arr[left]);
+                 z.add(arr[right]);
+                 nums.add(z);
                  left++;
+             right--;
+                 while(left<right&&arr[left]==arr[left-1]){
+                     left++;
+                 }
+                 while(left<right&&arr[right]==arr[right+1]){
+                     right--;
+                 }
+             }else if(sum<0){
+                 left++;
+             }else{
                  right--;
-                 while(left<right &&arr[left]==arr[left-1]){
-                     left++;
-                 }
-                 while(left<right &&arr[right]==arr[right+1]){
-                     right--;
-                 }
-                 }else if(sum<0){
-                     left++;
-                 
-                 }else{
-                     right--;
-                 }
              }
          }
-         return ans;
      }
-        
+       return nums; 
     }
-
+}
